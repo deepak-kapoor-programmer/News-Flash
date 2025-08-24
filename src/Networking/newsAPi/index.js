ab07447d04ccb925691b9f073796880e31d374f9
dk-codex <kapoordeepak636@gmail.com>
2025-08-24 14:04:59 +0530

    Final update

--- src/Networking/newsAPi/index.js
+++ src/Networking/newsAPi/index.js
@@ -7,26 +7,29 @@ export const FetchNews = async (keyword = "india", date = null, useTopHeadlines 
     // let url = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${apikey}`
     let url;
 
-    if (useTopHeadlines) {
-        // Top headlines (default home view)
-        // console.log("headline");
-        url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}`;
-    } else if (date) {
-        // console.log("date");
-        url = `https://newsapi.org/v2/everything?q=${keyword}&from=${date}&to=${date}&sortBy=popularity&apiKey=${apikey}`;
-    } else {
-        // console.log("keywords");
-        url = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=popularity&apiKey=${apikey}`;
-    }
     // if (date) {
     //     url = `https://newsapi.org/v2/everything?q=india&from=${date}&to=${date}&sortBy=publishedAt&apiKey=${apikey}`;
     // } else {
     //     url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apikey}`;
     // }
     try {
+
+        if (useTopHeadlines) {
+            // Top headlines (default home view)
+            // console.log("headline");
+            url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}`;
+        } else if (date) {
+            // console.log("date");
+            url = `https://newsapi.org/v2/everything?q=${keyword}&from=${date}&to=${date}&sortBy=popularity&apiKey=${apikey}`;
+        } else {
+            // console.log("keywords");
+            url = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=popularity&apiKey=${apikey}`;
+        }
         // console.log("url  ", url);
 
         const response = await fetch(url);
+        console.log("Api worked..");
+        
         const data = await response.json();
         // console.log("hello fardeen in index",data)
         return ({
